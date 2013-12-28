b11da2464c40465079b4e1e3b6d47b344447764d
bevk.jani <bevk.jani@gmail.com>
2013-12-28 17:59:18 +0100

    minor

--- LTTC/Assets/Scripts/CraterMaker.cs
+++ LTTC/Assets/Scripts/CraterMaker.cs
@@ -11,12 +11,16 @@ public class CraterMaker : MonoBehaviour {
 	private int m_HeightmapWidth;
 	private int m_HeightmapHeight;
 
+	private TextureChanger m_texChanger;
+
 	void Start () {
 		m_TerrainData = Terrain.activeTerrain.terrainData;
 		m_HeightmapWidth = m_TerrainData.heightmapWidth;
 		m_HeightmapHeight = m_TerrainData.heightmapHeight;
 		m_SavedTerrainData = m_TerrainData.GetHeights(0, 0, m_HeightmapWidth, m_HeightmapHeight);
 		m_CraterData = m_CraterTexture.GetPixels();
+
+		m_texChanger = Terrain.activeTerrain.GetComponent<TextureChanger>();
 	}
 	
 	void OnApplicationQuit () {
@@ -24,26 +28,21 @@ public class CraterMaker : MonoBehaviour {
 	}
 
 	public void MakeCrater( Vector3 impact ){
+		m_texChanger.MakeCrater( impact );
+
 		impact -= transform.position;
 
 		int x = (int)Mathf.Lerp(0, m_HeightmapWidth, Mathf.InverseLerp(0, m_TerrainData.size.x, impact.x));
 		int z = (int)Mathf.Lerp(0, m_HeightmapHeight, Mathf.InverseLerp(0, m_TerrainData.size.z, impact.z));
 
-
-		//Debug.Log(impact + " " + x + " " + z);
-
 		x = Mathf.Clamp(x, m_CraterTexture.width/2, m_HeightmapWidth-m_CraterTexture.width/2);
 		z = Mathf.Clamp(z, m_CraterTexture.height/2, m_HeightmapHeight-m_CraterTexture.height/2);
-
-		//Debug.Log("c# " + x + " " + z);
-		
 
 		float[,] areaT = m_TerrainData.GetHeights((int)(x-m_CraterTexture.width/2), (int)(z-m_CraterTexture.height/2), m_CraterTexture.width, m_CraterTexture.height);
 
 		for (int i = 0; i < m_CraterTexture.height; i++) {
 			for (int j = 0; j < m_CraterTexture.width; j++) {
 				areaT[i,j] = (float)(areaT[i,j] - m_CraterData[i*m_CraterTexture.width+j].a * 0.01);
-				//areaT[i,j] = 200;
 			}	
 		}
 		
---
+++ LTTC/Assets/TextureChanger.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TextureChanger : MonoBehaviour {
+	public Texture2D m_CraterTexture;
+
+	private Color[] m_CraterData;
+
+	private TerrainData m_TerrainData;
+	private float[,,] m_saved;
+
+	private int m_Width;	// Aplha map
+	private int m_Height;	// Aplha map
+	public int m_Layers;	// Aplha map
+
+	// Use this for initialization
+	void Start () {
+		m_TerrainData = Terrain.activeTerrain.terrainData;
+		m_Width = m_TerrainData.alphamapWidth;
+		m_Height = m_TerrainData.alphamapHeight;
+		m_Layers = m_TerrainData.alphamapLayers;
+		m_saved = m_TerrainData.GetAlphamaps(0, 0, m_Width, m_Height);
+		m_CraterData = m_CraterTexture.GetPixels();
+	}
+
+	void OnApplicationQuit () {
+		m_TerrainData.SetAlphamaps(0, 0, m_saved);
+	}
+
+	public void MakeCrater( Vector3 impact ){
+		impact -= transform.position;
+
+		Debug.Log ("tex");
+		int g = (int) Mathf.Lerp(0, m_Width, Mathf.InverseLerp(0, m_TerrainData.size.x, impact.x));
+		int b = (int) Mathf.Lerp(0, m_Width, Mathf.InverseLerp(0, m_TerrainData.size.z, impact.z));
+
+		g = Mathf.Clamp(g, m_CraterTexture.width/2, m_Width-m_CraterTexture.width/2);
+		b = Mathf.Clamp(b, m_CraterTexture.height/2, m_Width-m_CraterTexture.height/2);
+
+		float[,,] area = m_TerrainData.GetAlphamaps( g-m_CraterTexture.width/2, 
+		                                            b-m_CraterTexture.height/2, 
+		                                            m_CraterTexture.width, 
+		                                            m_CraterTexture.height );
+
+		for( int x = 0; x < m_CraterTexture.height; x++ ) {
+			for( int y = 0; y < m_CraterTexture.width; y++ ) {
+				for( int z = 0; z < m_Layers; z++){	
+					if (z == 1){
+						area[x,y,z] += m_CraterData[ x*m_CraterTexture.width + y ].a;
+					} else{	
+						area[x,y,z] -= m_CraterData[ x*m_CraterTexture.width + y ].a;	
+					}	
+				}
+			}
+		}
+
+		m_TerrainData.SetAlphamaps (g-m_CraterTexture.width/2,b-m_CraterTexture.height/2,area);
+	}
+}
