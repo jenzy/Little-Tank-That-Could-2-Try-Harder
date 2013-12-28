beebc68655856f6d7414e00479492554a5eb1418
bevk.jani <bevk.jani@gmail.com>
2013-12-28 17:15:10 +0100

    muzzle flash

--- LTTC/Assets/Scripts/GunFire.cs
+++ LTTC/Assets/Scripts/GunFire.cs
@@ -2,7 +2,9 @@ using UnityEngine;
 using System.Collections;
 
 public class GunFire : MonoBehaviour {
-	public GameObject mProjectile;
+	public GameObject m_Projectile;
+	public GameObject m_MuzzleFlash;
+
 	public float mInitialForce = 10000f;
 
 	void Start () {
@@ -12,9 +14,11 @@ public class GunFire : MonoBehaviour {
 	void Update () {
 		if( Input.GetButtonUp(In.BUTTON_FIRE) ){
 			Vector3 startPosition = GameObject.FindGameObjectWithTag(Tags.PROJECTILE_START).transform.position;
-			GameObject projectile = Instantiate(mProjectile, startPosition, Quaternion.identity) as GameObject;
+			GameObject projectile = Instantiate(m_Projectile, startPosition, Quaternion.identity) as GameObject;
 			Vector3 force = transform.rotation *  Vector3.forward * mInitialForce;
 			projectile.rigidbody.AddForce(force);
+
+			Instantiate(m_MuzzleFlash, startPosition, transform.rotation);
 		}
 	}
 }
