589cb3a24439054e0db58c47e68ddc24edddcab0
bevk.jani <bevk.jani@gmail.com>
2014-01-06 16:24:52 +0100

    lose screen

--- LTTC/Assets/Scripts/GUI/EndMenu.cs
+++ LTTC/Assets/Scripts/GUI/EndMenu.cs
@@ -2,23 +2,58 @@
 using System.Collections;
 
 public class EndMenu : MonoBehaviour {
+	public GUISkin guiSkin;
+
+
+	public GameObject m_MainText;
+	public GameObject m_SubText;
+	public GameObject m_LoseExplosion;
+	public GameObject m_Camera;
+
 	public enum EndMenuState {WIN, LOSE}
 	public static EndMenuState EndState {get; set;}
 
 	// Use this for initialization
 	void Start () {
+		EndState = EndMenuState.LOSE;
+
 		switch(EndState){
 		case EndMenuState.WIN:
-			this.guiText.text = "WIN";
+			m_MainText.guiText.text = "CONGRATULATIONS!";
+			m_SubText.guiText.text = "You have successfully foiled the enemy's plan!";
+			
 			break;
 		case EndMenuState.LOSE:
-			this.guiText.text = "LOSE";
+			m_MainText.guiText.text = "GAME OVER!";
+			m_SubText.guiText.text = "You failed, the enemy has defeated us!";
+			Invoke("LoseExplosion", 1);
+			Invoke("CameraUp", 6);
+
+			RenderSettings.skybox.color = new Color(0,0,0,0);
+
 			break;
 		}
 	}
+
+	void Update(){
+		if(Input.GetButtonUp(In.BUTTON_EXIT)){
+			Application.LoadLevel(Level.MENU);
+		}
+	}
 	
-	// Update is called once per frame
-	void Update () {
-	
+	void OnGUI(){
+		GUI.skin = guiSkin;
+		
+		if( GUI.Button(new Rect(Screen.width/2-75,Screen.height-50,150, 40), "Back") ){
+			Application.LoadLevel(Level.MENU);
+		}
+	}
+
+	private void CameraUp(){
+		m_Camera.animation.Play();
+	}
+
+	private void LoseExplosion(){
+		m_LoseExplosion.GetComponent<Detonator>().Explode();
 	}
 }
